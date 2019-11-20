a6c95ad29ccb69165494aa8c09de8d55175cd5ba
marvermar <m.veramartin@gmail.com>
2019-11-20 15:36:26 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const btn = document.querySelector('#btn');
+const input = document.querySelector('#input');
+const clue = document.querySelector('#clue');
+const attempts = document.querySelector('#attempts');
+
+
+
+
+
+
+btn.addEventListener('click')
