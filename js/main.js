ca520b9ea1d6ccaff20d729df65d07e8f96abe6a
marvermar <m.veramartin@gmail.com>
2019-11-20 16:42:29 +0100

    add some functions

--- js/main.js
+++ js/main.js
@@ -1,13 +1,52 @@
 'use strict'
 
 const btn = document.querySelector('#btn');
-const input = document.querySelector('#input');
-const clue = document.querySelector('#clue');
-const attempts = document.querySelector('#attempts');
+const input = document.querySelector('#input').value;
+const clueBox = document.querySelector('#clue');
+const attemptsBox = document.querySelector('#attempts');
 
 
+function increaseAttempts() {
+    let attempts = parseInt(attemptsBox.innerHTMl);
+    attempts = attempts + 1;
+    attemptsBox.innerHTML = attempts;
+    console.log('holaaaaaa')
+}
 
 
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+const randomNumber = getRandomNumber(100);
+
+function createClue() {
+    if (input.value === randomNumber) {
+        clueBox.innerHTML = '¡Has acertado campeona!'
+        alert: '¡¡ HAS ACERTADO  CAMPEONA!!';
+    }
+    if (input.value > randomNumber.value) {
+        clueBox.innerHTML = 'Demasiado alto';
+    }
+    if (input.value < randomNumber.value) {
+        clueBox.innerHTML = 'Demasiado bajo';
+    }
+}
+
+function clickButton() {
+    console.log(randomNumber)
+    increaseAttempts()
+    console.log(input.value);
 
 
-btn.addEventListener('click')
+}
+
+
+btn.addEventListener('click', clickButton);
+
+
+/*function increaseProductQuantity() {
+    let productQuantity = parseInt(elementWrapperCart.innerHTML);
+    productQuantity = productQuantity + 1;
+    elementWrapperCart.innerHTML = productQuantity;
+  }*/
